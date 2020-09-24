bfbab55c0cfa650ee65e70591ca51cc37562f96b
maya <amacrk@i.softbank.jp>
2020-09-24 13:09:14 +0900

    Initial Commit

---
+++ Pet App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pet App
+//
+//  Created by maya on 2020/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
